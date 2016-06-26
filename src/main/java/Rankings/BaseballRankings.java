a736a2c62e0345578d2bc255b030689640ae4e6b
ミキ Wawrzyniak <mikolajspejs@gmail.com>
2016-06-26 13:59:05 +0200

    Added rankings

---
+++ src/main/java/Rankings/BaseballRankings.java
@@ -0,0 +1,17 @@
+package Rankings;
+
+/**
+ * Created by Mikolaj Wawrzyniak
+ */
+public enum BaseballRankings {
+    baseball_daily_player_hitter_ranking ("baseball_daily_player_hitter_ranking"),
+    baseball_daily_player_pitcher_ranking ("baseball_daily_player_pitcher_ranking"),
+    baseball_daily_team_longest_game_ranking ("baseball_daily_team_longest_game_ranking"),
+    baseball_season_team_longest_game_ranking ("baseball_season_team_longest_game_ranking");
+
+    private final String value;
+
+    private BaseballRankings(String s) {
+        value = s;
+    }
+}
---
+++ src/main/java/Rankings/HockeyRankings.java
@@ -0,0 +1,14 @@
+package Rankings;
+
+/**
+ * Created by Mikolaj Wawrzyniak
+ */
+public enum HockeyRankings {
+    hockey_daily_player_skater_ranking ("hockey_daily_player_skater_ranking");
+
+    private final String value;
+
+    private HockeyRankings(String s) {
+        value = s;
+    }
+}
